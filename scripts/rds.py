15b6c8efa0eacc57af4e81f9b8fbe01db50473cf
RenoAndino <renatocvz2025@gmail.com>
2026-04-29 15:30:43 -0500

    Cambiando esquema de la tabla. Nuevas columnas:  porcentaje_retencion_iva_modificado, porcentaje_retencion_renta_modificado, campo_formulario_103_ir_modificado

--- scripts/rds.py
+++ scripts/rds.py
@@ -303,8 +303,11 @@ class RDS:
                 Column("campo_formulario_104_iva_modificado", String(50)),
                 Column("codigo_anexo_iva", String(50)),
                 Column("codigo_anexo_iva_modificado", String(50)),
+                Column("porcentaje_retencion_iva_modificado", String(30)),
+                Column("porcentaje_retencion_renta_modificado", String(30)),
                 Column("codigo_anexo_ir_modificado", String(10)),
                 Column("campo_formulario_103_ir", String(10)),
+                Column("campo_formulario_103_ir_modificado", String(10)),
                 Column("fecha_carga", DateTime),
             )
         if tipo == 0:
